4b013dea2b0ba89ddb3137228014cced27da808d
啪叽 <lbf911@126.com>
2024-09-18 23:10:32 +0800

    feat: update

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # english-learning
 
 Learn English related to computer science.
-
-some translations
-
-1. [使用现代纯 JavaScript 进行内存高效 DOM 操作的模式](https://github.com/kaisa911/english-learning/blob/master/weeklyTranslation/1.md)
---
+++ data/vocab.js
@@ -0,0 +1,21 @@
+const vocabs = [
+  {
+    word: 'job',
+    meaning: '工作，作业',
+    type: '名词',
+    notes: '一般是指工作，计算机里有 作业，批处理、定时任务的意思',
+    sentences: [
+      {
+        sentence: 'The system administrator scheduled a nightly job to backup all user data.',
+        translation: '系统管理员安排了一个夜间的定时任务来备份所有用户数据。',
+      },
+      {
+        sentence:
+          'I submitted my resume for printing, but it seems to be stuck in the print queue as a job',
+        translation: '我提交了我的简历去打印，但它似乎卡在打印队列中作为一个打印作业',
+      },
+    ],
+  },
+];
+
+const url = 'https://dict.youdao.com/dictvoice?audio=high&type=2';
